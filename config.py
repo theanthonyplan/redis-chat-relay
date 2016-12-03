5a995de7462e9d2871525c4426f67a85e6d47530
avg <theanthonyplan@gmail.com>
2016-12-03 08:54:34 -0500

    added config

---
+++ config.py
@@ -0,0 +1,36 @@
+API_KEYS = {
+  "twitch": {},
+  "hitbox": {},
+  "irc": {},
+  "discord": {},
+  "steam": "48D1471B4DEBD1ADD3D38C984FD61202"
+}
+
+
+SERVICES = {
+  "twitch": {
+        'username': '',
+        'password': '',
+        'channel': '#porpoise_pete'
+  },
+  "hitbox": {
+        'username': '',
+        'password': '',
+        'channel': ''
+  },
+  "irc": {
+        'username': '',
+        'password': '',
+        'channel': ''
+},
+  "discord": {
+        'username': '',
+        'password': '',
+        'channel': ''
+  },
+  "steam": {
+        'username': '',
+        'password': '',
+        'channel': ''
+  }
+}
